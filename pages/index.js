8a8485cdc175e649179c16a61056d61d553c72d4
siddharth17vaishnav <siddharth17vaishnav@gmail.com>
2023-02-12 15:44:54 +0530

    added Description Section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Hero from "@/components/Hero";
+import Description from "@/components/Description";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
           heading="VAMBOX"
           message="Enjoy Unlimited 360* Videos and Images in VR for FREE."
         />
+        <Description />
       </main>
     </>
   );
