abeaa67a5c41cbaebe49bad85b0a0b877aaf5ab3
siddharth <siddharth17vaishnav@gmail.com>
2023-02-19 21:20:03 +0530

    added modal in image-cmoponent

--- components/DropFile.jsx
+++ components/DropFile.jsx
@@ -6,22 +6,11 @@ export const DropFile = () => {
   const [error, setError] = useState();
   const router = useRouter();
   const onSubmit = (type) => {
-    console.log(type);
-    if (file && file?.length !== 0) {
-      if (
-        file.includes("youtube") ||
-        file.includes("ytube") ||
-        file.includes("yt.com")
-      ) {
-        setError("We dont accept youtube links, try another link");
-      } else {
-        setError("");
-        if (type === "image") {
-          router.push(`/stream-image?${file}`).then();
-        } else if (type === "video") {
-          router.push(`/stream-video?${file}`).then();
-        }
-      }
+    console.log("clicked")
+    if (type === "image") {
+      router.push("/stream-image").then();
+    } else if (type === "video") {
+      router.push(`/stream-video`).then();
     }
   };
   return (
@@ -35,15 +24,15 @@ export const DropFile = () => {
       </div>
       <div className=" mt-20 gap-[70px] flex flex-wrap justify-center mx-auto">
         <div>
-          <div className="parent-container h-40 w-76 cursor-pointer rounded-xl bg-gray-100 border-dashed border-2 border-gray-400 relative flex justify-center items-center relative">
-            <input
+          <div className="parent-container h-40 w-76 cursor-pointer rounded-xl bg-gray-100 border-dashed border-2 border-gray-400 relative flex justify-center items-center relative"  onClick={() => onSubmit("image")}>
+            {/* <input
               type="file"
               id="input-file"
               className="absolute inset-0 opacity-0 cursor-pointer"
               onChange={(e) => setFile(e.target.files[0].name)}
-            />
+            /> */}
             <div className="w-64 h-20 flex justify-center items-center">
-              <div className="mr-2">
+              <div className="mr-2"  onClick={() => onSubmit("image")}>
                 <svg
                   className="upload-icon bi bi-cloud-arrow-up-fill"
                   xmlns="http://www.w3.org/2000/svg"
@@ -55,7 +44,7 @@ export const DropFile = () => {
                   <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 5.146a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2z" />
                 </svg>
               </div>
-              <div>
+              <div  onClick={() => onSubmit("image")}>
                 <p className="text-gray-500 mb-1 text-lg file_name">
                   Upload your image
                 </p>
@@ -81,7 +70,6 @@ export const DropFile = () => {
               <button
                 className="items-center rounded-full px-8 py-1 border border-white font-semibold  hover:bg-white hover:text-black"
                 type="button"
-                onClick={() => onSubmit("image")}
               >
                 Go
               </button>
@@ -114,7 +102,6 @@ export const DropFile = () => {
                 <p className="text-gray-500 mb-1 text-lg file_name">
                   Upload your video
                 </p>
-                
               </div>
             </div>
           </div>
