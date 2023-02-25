67868e80f88fed3572a8a5551bda69e984371dfc
Siddharth <siddharth17vaishnav@gmail.com>
2023-02-25 10:29:52 +0530

    Setup React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-
+import {Routes,Route} from 'react-router-dom'
+import {HomeComponent} from './common/Path'
 function App() {
   
 
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<HomeComponent/>}/>
+      </Routes>
     </div>
   )
 }
